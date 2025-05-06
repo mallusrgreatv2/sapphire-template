6ad4d6ba70ca7dfb89da3a5c1941b3dda1a83792
mallusrgreatv2 <mallusrgreat@gmail.com>
2025-05-06 12:14:27 +0530

    chore: remove logged in log

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,8 @@ const client = new SapphireClient({
 
 const main = async () => {
   try {
-    client.logger.info("Logging in");
+    client.logger.info("Logging in...");
     await client.login();
-    client.logger.info("Logged in");
   } catch (error) {
     client.logger.fatal(error);
     await client.destroy();
